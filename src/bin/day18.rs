0193de274f4082c2d679836d9f3f0cebe476a919
msc94 <msc94@users.noreply.github.com>
2022-12-18 19:42:17 +0100

    Update day18

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -6,7 +6,7 @@ use core::fmt;
 use itertools::Itertools;
 use sscanf::regex::Regex;
 use std::cmp::{max, min};
-use std::collections::HashSet;
+use std::collections::{vec_deque, HashSet, VecDeque};
 use std::ops::{Range, RangeInclusive};
 use std::{collections::HashMap, error::Error};
 use utils::get_lines;
@@ -33,10 +33,7 @@ fn add_pos(a: &Pos, b: &Pos) -> Pos {
 }
 
 fn check_cubes(cubes: Cubes) -> i32 {
-    let mut added = Cubes::new();
-    let mut surrounded = Cubes::new();
     let mut total_surface_area = 0;
-    let mut hidden_surface_area = 0;
 
     let directions = vec![
         (1, 0, 0),
@@ -47,32 +44,51 @@ fn check_cubes(cubes: Cubes) -> i32 {
         (0, 0, -1),
     ];
 
-    for c in cubes {
-        println!("Adding {:?}", c);
+    let cubes: HashSet<Pos> = HashSet::from_iter(cubes.iter().cloned());
 
-        total_surface_area += 6;
+    // Flood from the outside
+    let max_x = cubes.iter().map(|x| x.0).max().unwrap() + 1;
+    let min_x = cubes.iter().map(|x| x.0).min().unwrap() - 1;
 
-        let neighbor_positions = directions.iter().map(|x| add_pos(&c, x));
-        for n in neighbor_positions {
-            if added.contains(&n) {
-                hidden_surface_area += 2;
+    let max_y = cubes.iter().map(|x| x.1).max().unwrap() + 1;
+    let min_y = cubes.iter().map(|x| x.1).min().unwrap() - 1;
+
+    let max_z = cubes.iter().map(|x| x.2).max().unwrap() + 1;
+    let min_z = cubes.iter().map(|x| x.2).min().unwrap() - 1;
+
+    let mut visited = HashSet::new();
+    let mut queue = VecDeque::new();
+    queue.push_back((min_x, min_y, min_z));
+
+    while !queue.is_empty() {
+        let current = queue.pop_front().unwrap();
+
+        if visited.contains(&current) {
+            continue;
+        }
+
+        visited.insert(current);
+        println!("Doing {current:?}");
+
+        let neighbors = directions.iter().map(|x| add_pos(&current, x));
+        for n in neighbors {
+            if cubes.contains(&n) {
+                total_surface_area += 1;
             } else {
-                // Check if air tile is surrounded
-                let is_surrounded = directions
-                    .iter()
-                    .map(|x| add_pos(&n, x))
-                    .all(|x| x == c || added.contains(&x));
-
-                if is_surrounded {
-                    surrounded.insert(n);
+                if n.0 >= min_x
+                    && n.0 <= max_x
+                    && n.1 >= min_y
+                    && n.1 <= max_y
+                    && n.2 >= min_z
+                    && n.2 <= max_z
+                {
+                    queue.push_back(n);
                 }
             }
         }
-
-        added.insert(c);
     }
 
-    return total_surface_area - hidden_surface_area - 6 * surrounded.len() as i32;
+    total_surface_area
 }
 
 fn main() -> Result<()> {
